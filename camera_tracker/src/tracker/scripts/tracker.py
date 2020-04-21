67de146cf752858b564807c8eea83fc8a17e9209
Homry <sorokumov.g.2019@fml31.ru>
2020-04-21 23:54:43 +0300

    change tracker.py

--- camera_tracker/src/tracker/scripts/tracker.py
+++ camera_tracker/src/tracker/scripts/tracker.py
@@ -2,15 +2,18 @@
 import cv2.aruco as aruco
 import rospy
 import numpy as np
-from sensor_msgs.msg import Image 
+from sensor_msgs.msg import Image
 
 MARKER_TYPE = aruco.DICT_5X5_250
 
-def tracker(image_sub, markerLength, CameraMtx, DistortionCoeff):
+def tracker(image_sub):
+    CameraMtx = [682.42294095, 0.0, 469.948484581, 0.0, 681.44128234, 333.951368692, 0.0, 0.0, 1.0]
+    DistortionCoeff = [0.0692332572094, -0.170327984628, 0.00436794532634, 0.00496921804668, 0.0]
+    markerLength = 0.1
     arucoDictionacy = aruco.Dictionary_get(MARKER_TYPE)
-    parametrs = aruco.DetectorParameters_create()
-    image_sub = np.array(image_sub)
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(image_sub, arucoDictionacy, parametrs=parametrs, cameraMatrix=CameraMtx, distCoeff=DistortionCoeff)
+    parameters = aruco.DetectorParameters_create()
+    #image_sub = np.array(image_sub)
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(image_sub, arucoDictionacy, parameters=parameters, cameraMatrix=CameraMtx, distCoeff=DistortionCoeff)
     for j in range(0, len(ids)):
         rvec, tvec, objPoints = aruco.estimatePoseSingleMarkers(corners[j], markerLength, CameraMtx, DistortionCoeff)
         aruco.drawDetectedMarkers(image_sub, corners, ids)
@@ -19,10 +22,10 @@ def tracker(image_sub, markerLength, CameraMtx, DistortionCoeff):
 
 
 if __name__ == '__main__':
-    CameraMtx = [682.42294095, 0.0, 469.948484581, 0.0, 681.44128234, 333.951368692, 0.0, 0.0, 1.0]
-    DistortionCoeff = [0.0692332572094, -0.170327984628, 0.00436794532634, 0.00496921804668, 0.0]
+    #CameraMtx = [682.42294095, 0.0, 469.948484581, 0.0, 681.44128234, 333.951368692, 0.0, 0.0, 1.0]
+    #DistortionCoeff = [0.0692332572094, -0.170327984628, 0.00436794532634, 0.00496921804668, 0.0]
     rospy.init_node('tracker')
-    markerLength = rospy.get_param('~size')
-    image_sub = rospy.Subscriber("camera_image", Image)
-    tracker(image_sub, markerLength, CameraMtx, DistortionCoeff)
-    rospy.spinOnes()
+    #markerLength = rospy.get_param('~size')
+    image_sub = rospy.Subscriber("camera_image", Image, tracker)
+
+    rospy.spin()
