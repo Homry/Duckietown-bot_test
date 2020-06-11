411ed5a85bd34a5638b7269c965ef80a8f8aca29
Homry <sorokumov.g.2019@fml31.ru>
2020-06-11 14:40:07 +0300

    add find coord

---
+++ camera_tracker/build/equations/cmake_install.cmake
@@ -0,0 +1,58 @@
+# Install script for directory: /home/homry/Projects/Robots/camera_tracker/src/equations
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/home/homry/Projects/Robots/camera_tracker/install")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/homry/Projects/Robots/camera_tracker/build/equations/catkin_generated/installspace/equations.pc")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/equations/cmake" TYPE FILE FILES
+    "/home/homry/Projects/Robots/camera_tracker/build/equations/catkin_generated/installspace/equationsConfig.cmake"
+    "/home/homry/Projects/Robots/camera_tracker/build/equations/catkin_generated/installspace/equationsConfig-version.cmake"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/equations" TYPE FILE FILES "/home/homry/Projects/Robots/camera_tracker/src/equations/package.xml")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/equations" TYPE PROGRAM FILES "/home/homry/Projects/Robots/camera_tracker/src/equations/scripts/equations.py")
+endif()
+
---
+++ camera_tracker/src/equations/scripts/equations.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import rospy
+import tensorflow.compat.v1 as tf
+import numpy as np
+from geometry_msgs.msg import Vector3
+
+
+
+poseArr = np.array([[0.7, 0.3], [0.7, 0.9], [0.1, 0.9]])
+
+def equations(vector3_msg):
+    sess = tf.InteractiveSession()
+    tf.disable_v2_behavior()
+    x_init = tf.initializers.random_uniform(minval=-3.0, maxval=3.0)
+    y_init = tf.initializers.random_uniform(minval=-3.0, maxval=3.0)
+
+    x = tf.get_variable('x', shape=1 ,initializer=x_init)
+    y = tf.get_variable('y', shape=1 ,initializer=y_init)
+
+    eq1 = x*x - 2*x*poseArr[0][0] + poseArr[0][0]*poseArr[0][0] + y*y - 2*y*poseArr[0][1] + poseArr[0][1]*poseArr[0][1] - edgeArr[0]*edgeArr[0]
+    eq2 = x*x - 2*x*poseArr[1][0] + poseArr[1][0]*poseArr[1][0] + y*y - 2*y*poseArr[1][1] + poseArr[1][1]*poseArr[1][1] - edgeArr[1]*edgeArr[1]
+    eq3 = x*x - 2*x*poseArr[2][0] + poseArr[2][0]*poseArr[2][0] + y*y - 2*y*poseArr[2][1] + poseArr[2][1]*poseArr[2][1] - edgeArr[2]*edgeArr[2]
+
+
+
+    loss = tf.losses.mean_squared_error([eq1, eq2, eq3],[(0.0,), (0.0,), (0.0,)]) # функция потерь
+    optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+
+    epsilon = 0.01
+
+    sess.run(tf.global_variables_initializer())
+    sess.run([x,y])
+    loss_v = sess.run([loss])
+    step=0
+
+
+    while loss_v[0] >= epsilon:
+        step = step+1
+        sess.run(optimizer)
+        sess.run([x, y])
+        loss_v = sess.run([loss])
+
+    print("Step: " + str(step) + " Loss: " + str(loss_v))
+    print("x = %f, x = %f" % (x.eval(), y.eval()))
+
+if __name__ == '__main__':
+
+    rospy.init_node('equations')
+    sub = rospy.Subscriber("vector", Vector3, equations)
+
+
