2377a3c39725c15b900b9ee91111f6d0f9d91d4c
Homry <sorokumov.g.2019@fml31.ru>
2020-06-12 19:23:38 +0300

    delete devel and build

--- camera_tracker/build/ip_camera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/homry/Projects/Robots/camera_tracker/src/ip_camera
-# Build directory: /home/homry/Projects/Robots/camera_tracker/build/ip_camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
